71caa62a57a5dd767c21e03e11584c8afc813dda
Leonardo Souza <leogsouza@gmail.com>
2018-09-08 00:45:58 -0300

    Add new data

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ConnectedResultsTable from './ConnectedResultsTable';
+import NewResultsForm from './NewResultsForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <ConnectedResultsTable />
+        <NewResultsForm />
       </div>
     );
   }
